6ec37412d686dbeddab08858ec3731c215ee422c
Voytovic <voytovic@gmail.com>
2024-06-10 20:21:00 +0300

    upd

--- src/server.js
+++ src/server.js
@@ -2,53 +2,16 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactsById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import createHttpError from 'http-errors';
 
-const PORT = env.PORT || 3000;
+const PORT = env('PORT', 3000);
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
-
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-
-      res.status(200).json({
-        status: 'success',
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Failed to retrieve contacts',
-        error: error.message,
-      });
-    }
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactsById(contactId);
-
-      res.status(200).json({
-        status: 'success',
-        message: 'Successfully found contact with id {contactId}!',
-        data: contact,
-      });
-    } catch (error) {
-      res.status(404).json({
-        status: 'error',
-        message: 'Failed to retrieve contacts',
-        error: error.message,
-      });
-    }
-  });
-
   app.use(
     pino({
       transport: {
@@ -57,14 +20,17 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('/', (req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  app.use('/api', contactsRouter);
+
+  app.use((req, res, next) => {
+    next(createHttpError(404, 'Route not found'));
   });
 
-  app.use((err, req, res) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      status: 'error',
+      message: err.message || 'Something went wrong',
+      data: err.data || null,
     });
   });
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,19 @@ export const getContactsById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContacts = async (contactData) => {
+  const contact = new ContactsCollection(contactData);
+  return await contact.save();
+};
+
+export const updateContact = async (contactId, updateData, options = {}) => {
+  return await ContactsCollection.findByIdAndUpdate(contactId, updateData, {
+    new: true,
+    ...options,
+  });
+};
+
+export const deleteContact = async (contactId) => {
+  return await ContactsCollection.findByIdAndDelete(contactId);
+};
