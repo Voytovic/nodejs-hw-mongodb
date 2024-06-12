bd4c0340c2656846d177bc38ac0e396708a201e9
Voytovic <voytovic@gmail.com>
2024-06-12 22:25:29 +0300

    1

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,18 @@
+export const parseIsFavourite = (isFavourite) => {
+  if (typeof isFavourite === 'boolean') {
+    return isFavourite;
+  } else if (typeof isFavourite === 'string') {
+    if (isFavourite.toLowerCase() === 'true') {
+      return true;
+    } else if (isFavourite.toLowerCase() === 'false') {
+      return false;
+    }
+  }
+  return undefined;
+};
+
+export const parseFilterParams = (query) => {
+  const { isFavourite } = query;
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+  return { isFavourite: parsedIsFavourite };
+};
---
+++ src/utils/parsePaginationParams.js
@@ -0,0 +1,19 @@
+const parseNumber = (number, defaultValue) => {
+  const parsedNumber = parseInt(number, 10);
+  if (isNaN(parsedNumber)) {
+    return defaultValue;
+  }
+  return parsedNumber;
+};
+
+export const parsePaginationParams = (query) => {
+  const { page, perPage } = query;
+
+  const parsedPage = parseNumber(page, 1);
+  const parsedPerPage = parseNumber(perPage, 10);
+
+  return {
+    page: parsedPage,
+    perPage: parsedPerPage,
+  };
+};
